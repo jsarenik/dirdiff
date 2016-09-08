8165a389e48c14ea4a42e41ba11d7bd38093d22a
Jan Sarenik <jajomojo@gmail.com>
2016-09-08 15:31:13 +0200

    v1: Works on Mac
    ./dirdiff.sh dira dirb | bzip2 > delta-b_to_a.bz2
    cd dira
    bzcat ../delta-b_to_a.bz2 | sh -sx

--- dirdiff.sh
+++ dirdiff.sh
@@ -1,39 +1,52 @@
 #!/bin/sh
 
-test $# -ne 2 && {
-  cat <<EOF
-Usage: $0 <directory_old> <directory_new>
-EOF
+test $# -eq 2 || {
+  cat <<-EOF
+	Usage: $0 <directory_old> <directory_new>
+	EOF
   exit 1
 }
 
 DIRA=$1
 DIRB=$2
+EOFMARK=EOOOFDIRDIFF
+
+mydon() {
+  dp=${1#$3/}
+  dp=${dp#$3}
+  dp=${dp%:}
+  dp=${dp:+"$dp/"}
+  echo $dp$2
+}
+
+mydel() {
+  echo "rm -rfv $1"
+}
+
+mydiff() {
+  diff -q $1 $2 >/dev/null 2>&1
+  case $? in
+    1)
+      echo 'patch -Np1 <<EOOOFDIRDIFF'
+      ! diff -u $1 $2
+      echo EOOOFDIRDIFF
+      ;;
+    2) myblob ${2#*/};;
+  esac
+}
+
+myblob() {
+  echo "base64 -D <<$EOFMARK | tar xv"
+  tar c -C $DIRB $1 | base64
+  echo "$EOFMARK"
+}
 
 diff -qr $DIRA $DIRB | while read a b c d e
 do
-  case $a in
-    Only)
-      unset dirpart
-      if
-        echo $c | grep -q /
-      then
-        dirpart=$(echo $c | tr -d : | cut -d/ -f2-)/
-      fi
-      if
-        echo $c | grep -q $DIRA
-      then
-        echo "rm -rfv $dirpart$d"
-      else
-	echo "base64 -D <<EOOOFDIRDIFF | tar xv"
-        tar c -C $DIRB $dirpart$d | base64
-        echo EOOOFDIRDIFF
-      fi
-      ;;
-    Files) echo 'patch -Np1 <<EOOOFDIRDIFF'
-      diff -du $b $d
-      echo EOOOFDIRDIFF
-      ;;
-    *) echo unknown;;
+  case "$a $b $c" in
+    Only\ in\ $DIRA*) mydel $(mydon $c $d $DIRA);;
+    Only\ in\ $DIRB*) myblob $(mydon $c $d $DIRB);;
+    Files\ *) mydiff $b $d;;
+    *) echo unknown line $a $b $c $d $e 1>&2; exit 1;;
   esac
 done
